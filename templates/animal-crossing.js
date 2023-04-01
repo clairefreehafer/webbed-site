3a3c929a616f98675619e9f8e3a4b54423edf996
Claire Freehafer <claire@clippn.com>
2023-04-01 15:30:26 -0400

    use grid

--- templates/animal-crossing.js
+++ templates/animal-crossing.js
@@ -1,9 +1,10 @@
 import handleErrors from "../modules/errorHandling.js";
 import { getGrassColor, getPageConfig } from "../modules/utils.js";
 
+// break apart into pieces to be reused in other generators
 async function generatePage(pageName, pageFolder) {
   let page = await getPageConfig(pageName, pageFolder);
-  const { date, name } = page;
+  const { date, name, images } = page;
 
   const jsDate = new Date(date[0], date[1], date[2]);
 
@@ -12,8 +13,34 @@ async function generatePage(pageName, pageFolder) {
 
   document.title = `${name} – claire freeahfer`;
 
+  const numberOfImages = images.length;
+
+  // image orientation
+  switch (numberOfImages) {
+    case 1:
+      // default
+      break;
+    case 2:
+      if (window.matchMedia("(orientation: landscape)").matches) {
+        body.style.gridTemplateColumns = "1fr 1fr";
+      }
+      break;
+    case 3:
+      // ???
+      break;
+    case 4:
+      if (window.matchMedia("(orientation: landscape)").matches) {
+        body.style.gridTemplateColumns = "1fr 1fr";
+        body.style.gridTemplateRows = "1fr 1fr";
+      } else {
+        body.style.gridTemplateColumns = "1fr";
+        body.style.gridTemplateRows = "1fr 1fr 1fr 1fr";
+      }
+      break;
+  }
+
   // images
-  page.images.forEach((image, i) => {
+  images.forEach((image, i) => {
     const imageEl = document.createElement("img");
 
     imageEl.src = image.src;
