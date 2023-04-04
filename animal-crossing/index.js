195cff125dab46d761ea639d7d245315b6afb1fe
Claire Freehafer <claire@clippn.com>
2023-04-04 14:09:40 -0400

    add sand ac background

--- animal-crossing/index.js
+++ animal-crossing/index.js
@@ -4,8 +4,13 @@ import { generatePageList, getGrassColor } from "../modules/utils.js";
 try {
   generatePageList("animal-crossing/new-leaf", "new-leaf");
 
+  const grassColor = getGrassColor("square");
+
   const body = document.querySelector("body");
-  body.style.backgroundImage = `url("/images/grass/${getGrassColor("square")}"`;
+  body.style.backgroundImage = `url("/images/grass/${grassColor}"`;
+
+  const textBackground = document.querySelector("#text-background");
+  textBackground.style.backgroundImage = `url("/images/sand/${grassColor}"`;
 } catch (e) {
   handleErrors(e);
 }
