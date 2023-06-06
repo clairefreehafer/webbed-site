e542d19eb5e7d4c5691189cfb38f233dd90ef2b6
Claire Freehafer <claire@clippn.com>
2023-06-06 17:47:57 -0400

    copy files into build folder

--- build.js
+++ build.js
@@ -1,11 +1,18 @@
 const { generatePhotographyPages } = require("./utils/build");
-const { rmSync, existsSync, mkdirSync } = require("fs");
+const { rmSync, existsSync, mkdirSync, copyFileSync } = require("fs");
 const path = require("node:path");
 
 if (existsSync(path.join(__dirname, "build"))) {
   rmSync(path.join(__dirname, "build"), { recursive: true });
 }
 mkdirSync(path.join(__dirname, "build"));
+
+// copy other static files for now
+const filesToCopy = ["index.html", "index.js", "index.css", "not_found.html", "style.css", "neocities.png"];
+
+for (const file of filesToCopy) {
+  copyFileSync(path.join(__dirname, file), path.join(__dirname, `build/${file}`));
+}
 
 generatePhotographyPages();
 
